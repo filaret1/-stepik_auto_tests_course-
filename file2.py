9d8fcfc6a57571c86b5c87fc9b2d1c7efb070bf3
Андрей <andreigovor77@gmail.com>
2022-01-23 16:38:19 +0300

    Add file2

---
+++ file2.py
@@ -0,0 +1,31 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import math
+import time
+
+
+browser = webdriver.Chrome()
+browser.get(" http://suninjuly.github.io/explicit_wait2.html")
+
+# говорим Selenium проверять в течение 12 секунд, пока кнопка не станет кликабельной
+button = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), '$100')
+    )
+btn = browser.find_element(By.ID, "book")
+btn.click()
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+try:
+    x_element = browser.find_element(By.ID, 'input_value')
+    x = int(x_element.text)
+    y = calc(x)
+    answer = browser.find_element(By.ID, 'answer')
+    answer.send_keys(y)
+    btn = browser.find_element(By.ID, 'solve')
+    btn.click()
+
+finally:
+    time.sleep(10)
